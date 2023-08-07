63c735fb392dae44b0f7aad70d38f36a4df6b9cc
aminulislam69 <119211802+aminulislam69@users.noreply.github.com>
2023-08-07 22:33:46 +0600

    login and registration page design done

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,8 +1,28 @@
 import React from 'react'
+import TextField from '@mui/material/TextField';
+import regLogo from '../assets/regLogo.png'
+import Button from '@mui/material/Button';
 
 const Login = () => {
   return (
-    <div>Login</div>
+    <>
+    <div className="regcontainer">
+      <div className="regbody">
+        <img className='regimg' src={regLogo}></img>
+        <h4 className='regheading'>Login</h4>
+        <p className='regpara'>Free register and you can enjoy it</p>
+        <div className="reginput">
+          <TextField id="outlined-basic" label="Email Addres" variant="outlined" />
+        </div>
+        <div className="reginput">
+          <TextField id="outlined-basic" label="Password" variant="outlined" />
+        </div>
+        <div className="regbutton">
+          <Button  variant="contained">Sign In</Button>
+        </div>
+      </div>
+    </div>
+</>
   )
 }
 
--- src/pages/Registration.jsx
+++ src/pages/Registration.jsx
@@ -1,8 +1,31 @@
 import React from 'react'
+import TextField from '@mui/material/TextField';
+import regLogo from '../assets/regLogo.png'
+import Button from '@mui/material/Button';
 
 const Registration = () => {
   return (
-    <div>Resistration</div>
+    <>
+        <div className="regcontainer">
+          <div className="regbody">
+            <img className='regimg' src={regLogo}></img>
+            <h4 className='regheading'>Get started with easily register</h4>
+            <p className='regpara'>Free register and you can enjoy it</p>
+            <div className="reginput">
+              <TextField id="outlined-basic" label="Email Addres" variant="outlined" />
+            </div>
+            <div className="reginput">
+              <TextField id="outlined-basic" label="Full name" variant="outlined" />
+            </div>
+            <div className="reginput">
+              <TextField id="outlined-basic" label="Password" variant="outlined" />
+            </div>
+            <div className="regbutton">
+              <Button  variant="contained">Sign up</Button>
+            </div>
+          </div>
+        </div>
+    </>
   )
 }
 
